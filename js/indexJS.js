af65efc0218d6f920ec2d43e7a89c06cc3e1bf90
MahmoudHeshamMohamed97 <mahmoud.hesham693@gmail.com>
2021-02-14 20:02:06 +0200

    changed into payment.html instead of test.html

--- js/indexJS.js
+++ js/indexJS.js
@@ -203,7 +203,7 @@ function bookTicket(){
             cost: cost
         }
         sessionStorage.setItem( "currentTravel", JSON.stringify(travelInfo) );
-        window.open("../paymentPage/test.html","_self");
+        window.open("../paymentPage/payment.html","_self");
     }
         
 
